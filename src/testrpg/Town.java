0db226b039dc3ffaf862799f327f0d9a2e8fae84
zkdn46 <123gusckd123@naver.com>
2024-11-14 18:24:14 +0900

    Add Town printMenu

--- src/testrpg/Town.java
+++ src/testrpg/Town.java
@@ -1,5 +1,21 @@
 package testrpg;
 
+import java.io.IOException;
+
 public class Town {
+	
+	protected void printMenu() {
+		Main.buffer.setLength(0);
+		Main.buffer.append("=== 더조은마을 === \n");
+		Main.buffer.append("1)길드 2)상점 3)인벤토리 4)저장 5)로드 6)뒤로가기\n");
+		try {
+			Main.writer.append(Main.buffer);
+			Main.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Main.input("메뉴 선택");
+	}
 
 }
