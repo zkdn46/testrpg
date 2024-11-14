ae43f92019ea547aec44f0e2dda1a807b2f82000
zkdn46 <123gusckd123@naver.com>
2024-11-14 18:24:35 +0900

    Add Meeple

--- src/testrpg/Meeple.java
+++ src/testrpg/Meeple.java
@@ -1,5 +1,14 @@
 package testrpg;
 
-public class Meeple {
-
+public class Meeple extends Unit {
+	protected int exp;
+	protected boolean isParty;
+	protected Item weapon;
+	protected Item armor;
+	protected Item ring;
+	
+	Meeple(String name, int level, int hp, int mp, int att, int def) {
+		super(name, level, hp, mp, att, def);
+	}
+	
 }
