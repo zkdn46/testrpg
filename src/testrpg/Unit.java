98a085aeaab5504c00e44956fd46b89f59788a6c
zkdn46 <123gusckd123@naver.com>
2024-11-14 18:24:00 +0900

    Add Unit

--- src/testrpg/Unit.java
+++ src/testrpg/Unit.java
@@ -1,5 +1,21 @@
 package testrpg;
 
-public class Unit {
+public abstract class Unit {
+	protected String name;
+	protected int hp;
+	protected int maxHp;
+	protected int mp;
+	protected int att;
+	protected int def;
+	protected int level;
 
+	Unit(String name, int level, int hp, int mp, int att, int def) {
+		this.name = name;
+		this.level = level;
+		this.hp = hp;
+		this.maxHp = hp;
+		this.mp = mp;
+		this.att = att;
+		this.def = def;
+	}
 }
