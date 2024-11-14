358e6955f2b22bdd29a6e268c84e489810e5657d
zkdn46 <123gusckd123@naver.com>
2024-11-14 18:24:23 +0900

    Add Monster

--- src/testrpg/Monster.java
+++ src/testrpg/Monster.java
@@ -1,5 +1,9 @@
 package testrpg;
 
-public class Monster {
+public class Monster extends Unit {
+	
+	Monster(String name, int level, int hp, int mp, int att, int def) {
+		super(name, level, hp, mp, att, def);
+	}
 
 }
