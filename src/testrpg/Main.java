55386a806e6ae91a2e595d91fabdb1ac4c71f9a9
zkdn46 <123gusckd123@naver.com>
2024-11-14 17:18:35 +0900

    Add printMenu()

--- src/testrpg/Main.java
+++ src/testrpg/Main.java
@@ -1,7 +1,39 @@
 package testrpg;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Main {
-	void run() {
-		
+	public StringBuffer buffer = new StringBuffer();
+	public BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+	public BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	private boolean isRun = true;
+	
+	public void run() {
+		while(isRun) {
+			printMenu();
+			play();
+		}
 	}
+
+	private void printMenu() {
+		buffer.append("=== test RPG === \n");
+		buffer.append("1)마을 2)던전 3)종료");
+		try {
+			writer.append(buffer);
+			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
