d79289de47c647c687de8796cdd1dd1d621a2c35
zkdn46 <123gusckd123@naver.com>
2024-11-15 10:12:51 +0900

    Add Dungeon

--- src/testrpg/Dungeon.java
+++ src/testrpg/Dungeon.java
@@ -1,10 +1,28 @@
 package testrpg;
 
+import java.io.IOException;
+
 public class Dungeon {
 
-	public void printMenu() {
-		System.out.println("던전 임당");
-		
+	protected Battle battle = new Battle();
+
+	protected void printMenu() {
+		Main.buffer.setLength(0);
+		Main.buffer.append("=== 던전 입구 === \n");
+		try {
+			Main.writer.append(Main.buffer);
+			Main.writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int sel = Main.input("1~10단계 던전 선택: ");
+
+		if (sel < 1 || sel > 10) {
+			System.err.println("1~10 선택!");
+			return;
+		}
+
+		battle.fight(sel);
 	}
 
 }
