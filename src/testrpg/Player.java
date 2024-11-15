0305cd72d4c2e3a4c9c0de4dfbea2def7f290f72
zkdn46 <123gusckd123@naver.com>
2024-11-15 13:57:08 +0900

    Add Player printItemList()

--- src/testrpg/Player.java
+++ src/testrpg/Player.java
@@ -1,5 +1,29 @@
 package testrpg;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	
+	private static Player instance = new Player();
+
+	public static Player getInstance() {
+		return instance;
+	}
+	
+	protected int money;
+	protected ArrayList<Item> items = new ArrayList<>();
+	protected ArrayList<Meeple> guilds = new ArrayList<>();
+	protected ArrayList<Meeple> partys = new ArrayList<>();
+	
+	protected void inventory(){
+		printItemList();
+	}
+	
+	private void printItemList() {
+	    int i = 1;
+		for (Item item : items) {
+			System.out.print(i++ + ".");
+			System.out.println(item);
+		}
+	  }
 }
