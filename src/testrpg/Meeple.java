52c9b1587fa9d58c3a5e2c6f6c6fe940a3482563
zkdn46 <123gusckd123@naver.com>
2024-11-15 15:56:43 +0900

    Add Meeple toString

--- src/testrpg/Meeple.java
+++ src/testrpg/Meeple.java
@@ -6,9 +6,14 @@ public class Meeple extends Unit {
 	protected Item weapon;
 	protected Item armor;
 	protected Item ring;
-	
+
 	Meeple(String name, int level, int hp, int mp, int att, int def) {
 		super(name, level, hp, mp, att, def);
 	}
-	
+
+	public String toString() {
+		return String.format("이름) %s / Lv.%d / HP:%d / MP:%d / ATT:%d / DEF:%d\n무기:%s / 방어구:%s / 장신구:%s\n", name, level,
+				hp, mp, att, def, weapon, armor, ring);
+	}
+
 }
